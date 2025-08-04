0dec44bd089ebd6b62391cf7510a6d6fd257d60f
Surajit Das <dassurajit59@yahoo.in>
2025-08-04 18:04:00 +0530

    js day-1

---
+++ javascript/js/js-1.js
@@ -0,0 +1,28 @@
+// console.log(100)
+
+function hello(){
+    console.log("Hello...")
+}
+hello()
+
+function add(a, b){
+    let c = a+b;
+    console.log("Sum = "+c)
+}
+
+let x = 100;
+let y = 30;
+add(x, y)
+
+function product(x, y){
+    return x*y;
+}
+
+let p = product(4, 5)
+console.log(p)
+
+
+function greet(name){
+    console.log("Hello "+name)
+}
+greet("Ram")
